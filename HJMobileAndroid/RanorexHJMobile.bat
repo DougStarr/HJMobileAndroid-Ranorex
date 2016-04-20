5fc99eacdbd459375235ec2e19525c45e7e2279d
DougStarr <doug.starr@hcss.com>
2016-04-20 15:20:44 -0500

    Initial Commit

---
+++ HJMobileAndroid/RanorexHJMobile.bat
@@ -0,0 +1,4 @@
+cd C:\
+"C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe" /command:update /path:"C:\RanorexStudioProjects\HJMobileAndroid" /closeonend:2
+
+start C:\RanorexStudioProjects\HJMobileAndroid\trunk\HJMobile\HJMobile\bin\Debug\HJMobileAndroid.exe /rf:"\\san01\VDrive\Automation Reports/HJMobileAndroid/%%S_%%Y%%M%%D_%%T.rxlog"
